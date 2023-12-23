74799ff2339f3f26388e530535d335aa365d897a
qzh <2806894439@qq.com>
2023-12-23 18:38:35 +0800

    RedisTemplate测试

--- spring_data_redis-demo/src/test/java/com/qzh/SpringDataRedisDemoApplicationTests.java
+++ spring_data_redis-demo/src/test/java/com/qzh/SpringDataRedisDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.qzh;
 
+import com.qzh.pojo.User;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SpringDataRedisDemoApplicationTests {
+    //@Autowired
+    @Resource(name = "redisTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
-    void contextLoads() {
+    void testString() {
+        //写入一条String数据
+        redisTemplate.opsForValue().set("name","虎哥");
+        //获取string数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
     }
 
+    @Test
+    void testSaveUser() {
+        //写入数据
+        redisTemplate.opsForValue().set("user:100", new User("虎哥", 21));
+        //读取数据
+        User user = (User) redisTemplate.opsForValue().get("user:100");
+        System.out.println("user = " + user);
+    }
 }
