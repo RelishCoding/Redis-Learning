804dc608af99774c5f1072fe4ed24f0db22c82ec
qzh <2806894439@qq.com>
2024-03-19 11:47:53 +0800

    初始工程-服务类

---
+++ hm-dianping/src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -0,0 +1,13 @@
+package com.hmdp.service;
+
+import com.hmdp.entity.VoucherOrder;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ */
+public interface IVoucherOrderService extends IService<VoucherOrder> {
+
+}
