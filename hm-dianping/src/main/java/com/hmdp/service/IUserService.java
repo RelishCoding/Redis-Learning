20abe0fe2bd41175ebc48a544f6f9a4c9fc4163e
qzh <2806894439@qq.com>
2024-03-21 17:40:32 +0800

    基于session的短信验证码登录注册

--- hm-dianping/src/main/java/com/hmdp/service/IUserService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IUserService.java
@@ -1,7 +1,11 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -9,5 +13,7 @@ import com.hmdp.entity.User;
  * </p>
  */
 public interface IUserService extends IService<User> {
+    Result sendCode(String phone, HttpSession session);
 
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
