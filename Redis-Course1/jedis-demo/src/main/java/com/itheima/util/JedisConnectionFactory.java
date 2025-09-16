a529c939dcfac727502d3e0d8ca5179e80c403e3
qzh <2806894439@qq.com>
2025-09-16 23:50:43 +0800

    Jedis的使用

---
+++ Redis-Course1/jedis-demo/src/main/java/com/itheima/util/JedisConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.itheima.util;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisConnectionFactory {
+    private static final JedisPool jedisPool;
+
+    static {
+        // 配置连接池
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        // 设置连接池参数
+        poolConfig.setMaxTotal(8);
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(0);
+        poolConfig.setMaxWaitMillis(1000);
+        // 创建连接池对象,参数：连接池配置、服务端ip、服务端端口、超时时间、密码
+        jedisPool = new JedisPool(poolConfig,
+                "192.168.221.140", 6379, 1000, "123456");
+    }
+
+    public static Jedis getJedis() {
+        return jedisPool.getResource();
+    }
+}
