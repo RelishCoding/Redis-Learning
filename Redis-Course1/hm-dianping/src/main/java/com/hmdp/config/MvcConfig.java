0a44d075368e24bcca5a2a29f4f98b4c80804e2b
qzh <2806894439@qq.com>
2025-09-18 00:41:07 +0800

    解决登录状态刷新问题

--- Redis-Course1/hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -25,6 +26,10 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/shop-type/**",
                         "upload/**",
                         "/voucher/**"
-                );
+                ).order(1);
+
+        // 刷新token拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate))
+                .addPathPatterns("/**").order(0);
     }
 }
--- Redis-Course1/hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -48,8 +48,15 @@ public class LoginInterceptor implements HandlerInterceptor {
         return true;
     }
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    /**
+     * 改进版preHandle方法，基于token和Redis
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    public boolean preHandle_new(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 1.获取请求头中的token
         String token = request.getHeader("authorization");
         if (StrUtil.isBlank(token)) {
@@ -83,8 +90,15 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // 移除用户
-        UserHolder.removeUser();
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() ==  null) {
+            // 没有，需要拦截，设置状态码
+            response.setStatus(401);
+            // 拦截
+            return false;
+        }
+        // 有用户，则放行
+        return true;
     }
 }
