5e2180563ce3c96de0c0fa883c978f561bf87403
qzh <2806894439@qq.com>
2025-09-17 17:17:02 +0800

    实现短信登录和注册功能

--- Redis-Course1/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.hmdp.controller;
 
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -27,5 +28,15 @@ public class UserController {
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // 发送短信验证码并保存验证码
         return userService.sendCode(phone, session);
+    }
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
+        // 实现登录功能
+        return userService.login(loginForm, session);
     }
 }
