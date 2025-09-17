7c97a1c2df06df41cdd1eddde08cebc3cdc114fb
qzh <2806894439@qq.com>
2025-09-17 16:55:52 +0800

    实现模拟发送短信验证码功能

---
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.hmdp.controller;
+
+import com.hmdp.dto.Result;
+import com.hmdp.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private IUserService userService;
+
+    /**
+     * 发送手机验证码
+     */
+    @PostMapping("/code")
+    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
+        // 发送短信验证码并保存验证码
+        return userService.sendCode(phone, session);
+    }
+}
---
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/utils/RegexPatterns.java
@@ -0,0 +1,6 @@
+package com.hmdp.utils;
+
+public class RegexPatterns {
+    // 手机号正则
+    public static final String PHONE_REGEX = "^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$";
+}
