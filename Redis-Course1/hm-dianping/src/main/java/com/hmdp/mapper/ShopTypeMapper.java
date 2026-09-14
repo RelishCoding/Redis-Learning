0456115e45a48d08276a022e13409390a6e71ec2
qzh <2806894439@qq.com>
2026-09-14 14:53:07 +0800

    给店铺类型查询业务添加缓存

---
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/mapper/ShopTypeMapper.java
@@ -0,0 +1,12 @@
+package com.hmdp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmdp.entity.ShopType;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ */
+public interface ShopTypeMapper extends BaseMapper<ShopType> {
+}
