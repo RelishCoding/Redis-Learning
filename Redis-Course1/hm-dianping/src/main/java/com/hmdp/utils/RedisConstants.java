a472dafdd1069376da5651bece4d26c746e4ebdb
qzh <2806894439@qq.com>
2026-09-14 16:15:50 +0800

    新增店铺相关常量

--- Redis-Course1/hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ Redis-Course1/hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -1,9 +1,19 @@
 package com.hmdp.utils;
 
 public class RedisConstants {
-    public static final String LOGIN_CODE_KEY = "login:code:";
+    public static final String LOGIN_CODE_KEY = "hmdp:login:code:";
     public static final Long LOGIN_CODE_TTL = 5L;
 
-    public static final String LOGIN_USER_KEY = "login:token:";
-    public static final Long LOGIN_USER_TTL = 30L;
+    public static final String LOGIN_USER_KEY = "hmdp:login:token:";
+    public static final Long LOGIN_USER_TTL = 60L;
+
+    public static final String SHOP_INFO_KEY = "hmdp:shop:info:";
+    public static final Long SHOP_INFO_TTL = 30L;
+
+    public static final String SHOP_TYPE_KEY = "hmdp:shop:type";
+
+    public static final Long CACHE_NULL_TTL = 2L;
+
+    public static final String LOCK_SHOP_KEY = "hmdp:lock:shop:";
+    public static final Long LOCK_SHOP_TTL = 10L;
 }
